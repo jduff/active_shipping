5d0eaeeb3f9957812459ce4f4975a8cf329245a4
John Duff <john.duff@jadedpixel.com>
2011-09-24 11:15:26 -0400

    Fix Rails 3.1 deprecation warnings

--- lib/vendor/quantified/lib/quantified.rb
+++ lib/vendor/quantified/lib/quantified.rb
@@ -1,6 +1,12 @@
 $: << File.dirname(__FILE__)
 
-require 'active_support/all'
+begin
+  require 'active_support/all'
+rescue LoadError => e
+  require 'rubygems'
+  gem "activesupport", ">= 2.3.5"
+  require "active_support/all"
+end
 require 'bigdecimal'
 
 require 'quantified/attribute'
