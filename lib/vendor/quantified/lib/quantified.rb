a0810051362837678ee15b00ca160e58f6575017
John Duff <john.duff@jadedpixel.com>
2011-09-25 14:46:08 -0400

    Move quantified active_support require to test helper

--- lib/vendor/quantified/lib/quantified.rb
+++ lib/vendor/quantified/lib/quantified.rb
@@ -1,12 +1,4 @@
 $: << File.dirname(__FILE__)
-
-begin
-  require 'active_support/all'
-rescue LoadError => e
-  require 'rubygems'
-  gem "activesupport", ">= 2.3.5"
-  require "active_support/all"
-end
 require 'bigdecimal'
 
 require 'quantified/attribute'
